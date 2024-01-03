d17925e1ac92615ec3ade02c496c9abdbe604c40
Naman-jain-01 <130527580+Naman-jain-01@users.noreply.github.com>
2024-01-03 16:37:14 +0530

    GFG_POTD_1_1_2024.java

--- JANUARY 2024/GFG_POTD_1_1_2024.java
+++ JANUARY 2024/GFG_POTD_1_1_2024.java
@@ -1,14 +1,4 @@
-package JANUARY;
 
-import java.sql.SQLOutput;
-import java.util.ArrayList;
-
-public class GFG_POTD_1_1_2024 {
-    public static void main(String[] args) {
-        int arr[] = {9, 3, 5, 7,2,1};
-        int k = 6;
-        System.out.println(Solution.canPair(arr,k));
-    }}
         class Solution {
             static public boolean canPair(int[] nums, int k) {
                int n=nums.length;
