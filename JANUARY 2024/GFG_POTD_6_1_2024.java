1bf64b13f5d07e47b461d2265a56603e4d43d581
Naman-jain-01 <130527580+Naman-jain-01@users.noreply.github.com>
2024-01-06 17:36:47 +0530

    Add files via upload

---
+++ JANUARY 2024/GFG_POTD_6_1_2024.java
@@ -0,0 +1,31 @@
+package JANUARY;
+
+class Solution7 {
+
+    static long primefactorsum(long n) {
+        long sum = 0;
+        while (n % 2 == 0) {
+            sum++;
+            n = n / 2;
+        }
+
+        for (int i = 3; i <= Math.sqrt(n); i = i + 2) {
+            while (n % i == 0) {
+                sum++;
+                n = n / i;
+            }
+        }
+        if (n > 2) sum++;
+        return sum;
+    }
+
+    public static long sumOfPowers(long a, long b) {
+        long ans = 0;
+        while (a <= b) {
+            ans += primefactorsum(a);
+            a++;
+        }
+        return ans;
+    }
+}
+    
