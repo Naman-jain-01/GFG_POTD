08229c76fe55525404795d3943c0c45feefe1831
Naman-jain-01 <130527580+Naman-jain-01@users.noreply.github.com>
2024-01-10 15:54:23 +0530

    Add files via upload

---
+++ JANUARY 2024/GFG_POTD_10_1_2024.java
@@ -0,0 +1,23 @@
+package JANUARY;
+import java.util.HashMap;
+
+class Solution10{
+int longSubarrWthSumDivByK(int a[], int n, int k)
+{
+    int[] prefsum=new int[n+1];
+    HashMap<Integer,Integer> map=new HashMap<>();
+    int maxlength=0;
+
+    for(int i=1;i<n+1;i++)
+    {
+        prefsum[i]=prefsum[i-1]+a[i-1];
+        int currem=(prefsum[i]%k + k )%k;
+
+        if(currem==0) maxlength=i;
+        else if( map.containsKey(currem)) maxlength=Math.max(maxlength,i-map.get(currem));
+        if( !map.containsKey(currem)) map.put(currem,i);
+    }
+    return maxlength;
+}
+
+}
