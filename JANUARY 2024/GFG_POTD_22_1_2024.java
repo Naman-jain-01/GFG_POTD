32f901dcc00917a759f61492b878456dfd243c93
Naman-jain-01 <130527580+Naman-jain-01@users.noreply.github.com>
2024-01-22 08:11:18 +0530

    Add files via upload

---
+++ JANUARY 2024/GFG_POTD_22_1_2024.java
@@ -0,0 +1,29 @@
+package JANUARY;
+import java.util.*;
+class Solution22
+{
+    public static void recursion(Node node , int sum , int curr_sum , ArrayList<Integer> path ,  ArrayList<ArrayList<Integer>> ans)
+    {
+        if(node==null) return;
+        curr_sum+=node.data;
+        path.add(node.data);
+        if(curr_sum==sum)
+        {
+            ans.add(new ArrayList<>(path));
+        }
+
+        if(node.left!=null) recursion(node.left,sum,curr_sum,path,ans);
+        if(node.right!=null) recursion(node.right,sum,curr_sum,path,ans);
+
+        int index_To_remove_from_path=path.size()-1;
+        path.remove(index_To_remove_from_path);
+
+    }
+    public static ArrayList<ArrayList<Integer>> printPaths(Node root, int sum)
+    {
+        ArrayList<Integer> path= new ArrayList<>() ;
+        ArrayList<ArrayList<Integer>> ans= new ArrayList<>() ;
+        recursion(root,sum,0,path,ans);
+        return ans;
+    }
+}
